80713239633fdd039e3c9425601402aed2eb02b3
Lee O'Keefe <okeefe.lee@gmail.com>
2017-09-13 14:21:59 -0700

    try this timeout method again

--- lib/twitter/streaming/connection.rb
+++ lib/twitter/streaming/connection.rb
@@ -5,42 +5,32 @@ require 'resolv'
 module Twitter
   module Streaming
     class Connection
-      attr_reader :tcp_socket_class, :ssl_socket_class, :keepalive
-
-      DEFAULT_KEEPALIVE_SETTINGS = {
-        idle_timeout: 60,
-        interval: 10,
-        count: 6,
-      }.freeze
-
       def initialize(opts = {})
         @tcp_socket_class = opts.fetch(:tcp_socket_class) { TCPSocket }
         @ssl_socket_class = opts.fetch(:ssl_socket_class) { OpenSSL::SSL::SSLSocket }
-        @keepalive = DEFAULT_KEEPALIVE_SETTINGS.merge(opts.fetch(:keepalive) { {enabled: false} })
+        @select_timeout = opts.fetch(:select_timeout) { 90 }
       end
+      attr_reader :tcp_socket_class, :ssl_socket_class, :select_timeout
 
-      def stream(request, response)
+      def stream(request, response) # rubocop:disable Metrics/MethodLength
         client_context = OpenSSL::SSL::SSLContext.new
-        client         = new_tcp_socket(request.socket_host, request.socket_port)
+        client         = @tcp_socket_class.new(Resolv.getaddress(request.uri.host), request.uri.port)
         ssl_client     = @ssl_socket_class.new(client, client_context)
 
         ssl_client.connect
         request.stream(ssl_client)
-        while body = ssl_client.readpartial(1024) # rubocop:disable AssignmentInCondition
-          response << body
-        end
-      end
-
-    private
-
-      def new_tcp_socket(host, port)
-        @tcp_socket_class.new(Resolv.getaddress(host), port).tap do |socket|
-          # Check that Socket::TCP_KEEPIDLE is present, so we know we can access these socket options
-          if @keepalive[:enabled] && defined?(Socket::TCP_KEEPIDLE)
-            socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
-            socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPIDLE, @keepalive[:idle_timeout])
-            socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPINTVL, @keepalive[:interval])
-            socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPCNT, @keepalive[:count])
+        loop do
+          begin
+            body = ssl_client.read_nonblock(1024)
+            response << body
+          rescue Errno::EAGAIN
+            readables, _, _ = IO.select([ssl_client], [], [], @select_timeout)
+            if readables.nil?
+              ssl_client.close
+              raise Twitter::Error::ServerError.new('Streaming timeout')
+            else
+              retry
+            end
           end
         end
       end
